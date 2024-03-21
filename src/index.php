be5bb85f9a8e1bb9a7aa3e06ac4956bee555fd76
JuanX_B <bertoia114@gmail.com>
2024-03-21 00:09:05 -0300

    pdo connection

--- src/index.php
+++ src/index.php
@@ -2,4 +2,21 @@
 
 require 'helpersFunctions.php';
 
-require 'router.php';
+//require 'router.php';
+
+
+// Connect with msql database
+
+$dns = "msql:host=localhost;port=3306;dbname=mydatabase;user=user;password=password;charset=utf8mb64";
+
+$pdo = new PDO($dns);
+
+$stm = $pdo->prepare("select * from post");
+
+$stm->execute();
+
+$posts = $stm->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($posts as $post) {
+    echo '<li>'.$post['title'].'</li>';
+}
