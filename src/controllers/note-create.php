ca9786ef8f1b9c0d132b2d50cf60539540913b18
JuanX_B <bertoia114@gmail.com>
2024-04-17 20:40:09 -0300

    Intro to Form Validations

--- src/controllers/note-create.php
+++ src/controllers/note-create.php
@@ -3,13 +3,26 @@ $heading = 'New Note';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
+    $body = trim($_POST['body']);
+    $errors = [];
 
-    $config = require 'config.php';
-    $db = new Database($config['database']);
+    if (strlen($body) === 0) {
+        $errors['body'] = 'The body is required';
+    }
+
+    if (strlen($body) > 1000) {
+        $errors['body'] = 'The body cant not be more than 1,000 characters.';
+    }
+
+    if (empty($errors)) {
+        $config = require 'config.php';
+        $db = new Database($config['database']);
 
 
-    $query = 'INSERT INTO notes(body, user_id) VALUES (:body,:user_id)';
-    $note = $db->query($query, ['body' => $_POST['body'], 'user_id' => 1 ]);
+        $query = 'INSERT INTO notes(body, user_id) VALUES (:body,:user_id)';
+        $db->query($query, ['body' => $body, 'user_id' => 1]);
+        $body ='';
+    }
 
 }
 
--- src/views/note-create.view.php
+++ src/views/note-create.view.php
@@ -7,18 +7,18 @@
 <main>
     <div class="mx-auto max-w-4xl py-6 sm:px-6 lg:px-8">
         <div>
-            <div class="md:grid md-grid-cols-3 md:gap-6">
-                <div class="mt-5 md:col-span-2 md:mt-0">
-                    <form action="#" method="POST">
-                        <div class="space-y-6 bg-white px-4 py-5 sm:p-6">
-                            <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                                <div class="col-span-full">
-                                    <label for="body"
-                                        class="block text-sm font-medium leading-6 text-gray-900">Body</label>
-                                    <div class="mt-2">
-                                        <textarea id="body" name="body" rows="3"
-                                            class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
-                                    </div>
+            <div class="mt-5 md:mt-0">
+                <form action="#" method="POST">
+                    <div class="space-y-6 bg-white px-4 py-5 sm:p-6">
+                        <div>
+                            <div class="col-span-full">
+                                <label for="body" class="block text-sm font-medium leading-6 text-gray-900">Body</label>
+                                <div class="mt-2">
+                                    <textarea id="body" name="body" rows="3"
+                                        class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"><?= htmlspecialchars($body ?? '') ?></textarea>
+                                    <?php if (isset($errors['body'])): ?>
+                                        <p class="text-red-500 text-xs mt-2"><?= $errors['body'] ?></p>
+                                    <?php endif; ?>
                                 </div>
                             </div>
                         </div>
@@ -27,10 +27,10 @@
                             <button type="submit"
                                 class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
                         </div>
-                    </form>
-                </div>
+                </form>
             </div>
         </div>
+    </div>
     </div>
 
 </main>
